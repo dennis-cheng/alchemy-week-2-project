16fbc19e9999a9434e8099c6c8cb8d154384d1e6
Dennis Cheng <dennischeng@hotmail.co.za>
2023-05-27 17:42:32 +0200

    Complete

--- server/index.js
+++ server/index.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express")
+const verifyProof = require("../utils/verifyProof")
+const MerkleTree = require("../utils/MerkleTree")
+const niceList = require("../utils/niceList.json")
+const { hexToBytes, bytesToHex } = require("ethereum-cryptography/utils")
 
-const port = 1225;
+const port = 1225
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 // TODO: hardcode a merkle root here representing the whole nice list
+// const merkleTree = new MerkleTree(niceList)
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa"
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
+  // TODO: prove that a name is in the list
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT)
+  if (isInTheList) {
+    res.send("You got a toy robot!")
+  } else {
+    res.send("You are not on the list :(")
   }
-  else {
-    res.send("You are not on the list :(");
-  }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+  console.log(`Listening on port ${port}!`)
+})
